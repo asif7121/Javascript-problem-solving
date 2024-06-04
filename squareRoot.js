7ddb5631dfc4cbb349f8401bb339dc74cb1e862f
Asif <ansariasif449@gmail.com>
2024-06-04 11:02:04 +0530

    square-root function added
    Signed-off-by: Asif <ansariasif449@gmail.com>

---
+++ squareRoot.js
@@ -0,0 +1,18 @@
+function findSquareRoot(number) {
+  if (typeof number !== "number") {
+    return "The input is not a number";
+  }
+  if (number < 0) {
+    return "Square root of negative numbers is not real";
+  }
+  return Math.sqrt(number);
+}
+
+// Example 
+let input = 16;
+let squareRoot = findSquareRoot(input);
+console.log(`The square root of ${input} is ${squareRoot}`);
+
+input = "hello";
+squareRoot = findSquareRoot(input);
+console.log(`The result for the input "${input}" is: ${squareRoot}`);
