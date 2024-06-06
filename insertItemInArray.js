4acb8b0ccb414b4fdfe61058d86b3db428ea4095
Asif <ansariasif449@gmail.com>
2024-06-06 17:43:59 +0530

    added loogic to insert a item in array
    Signed-off-by: Asif <ansariasif449@gmail.com>

---
+++ insertItemInArray.js
@@ -0,0 +1,9 @@
+// inserting element in an array by push method
+const fruits = [ "apple", "banana" ];
+fruits.push("orange");
+console.log( fruits ); 
+
+// inserting element in an array by using spread operator
+const arr = ['apple', 'banana'];
+const newArr = ['kiwi', ...arr, 'orange'];
+console.log(newArr); 
