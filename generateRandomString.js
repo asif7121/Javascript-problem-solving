352ae480f4918bddc434417a4b5fce1b11a66478
Asif <ansariasif449@gmail.com>
2024-06-07 16:38:46 +0530

    added func to generate random string
    Signed-off-by: Asif <ansariasif449@gmail.com>

---
+++ generateRandomString.js
@@ -0,0 +1,17 @@
+function generateRandomString(length) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+  let result = "";
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charactersLength);
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
+}
+
+
+let randomString = generateRandomString(10);
+console.log(randomString);
